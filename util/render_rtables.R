846d93abe40eb09a2922534a9a796b528d3efe1c
bz1165 <162563350+bz1165@users.noreply.github.com>
2026-03-24 01:33:03 -0400

    Update render_rtables.R

--- util/render_rtables.R
+++ util/render_rtables.R
@@ -1,8 +1,18 @@
 suppressPackageStartupMessages({
-  library(rtables)
   library(htmltools)
 })
 
 render_table_html <- function(tbl) {
-  HTML(as_html(tbl))
+  txt <- paste(capture.output(print(tbl)), collapse = "\n")
+
+  tags$pre(
+    style = paste(
+      "white-space: pre-wrap;",
+      "font-family: Menlo, Monaco, Consolas, monospace;",
+      "font-size: 12px;",
+      "line-height: 1.35;",
+      "margin: 0;"
+    ),
+    txt
+  )
 }
